1a493f092e979233f0667de29b36197cfeb62333
hasanrakibgit <rakibul.tqs@gmail.com>
2021-01-22 22:27:29 +0600

    final code

--- assignment.js
+++ assignment.js
@@ -1,36 +1,51 @@
 
-function kilometerToMeter(km){
-    var meter=km*1000;
+//https://github.com/hasanrakibgit/assignment.js
+
+function kilometerToMeter(km){              // KilometterToMeter function
+    if(km > 0){
+        var meter= km*1000;
     return meter;
+    }else{
+        console.log('You Have Enter invalid input type')
+    }   
 }
 
-function budgetCalculator(clock, phone, laptop ){
-    var result= clock*50 + phone*100 + laptop*500;
+
+function budgetCalculator(clock, phone, laptop ){   // budgetCalculator function
+    if(clock, phone, laptop >= 0){
+        var result= clock*50 + phone*100 + laptop*500;
      return result;
+    }else{
+        console.log('Please Enter valid number of three input')
+    }
 }
+
 
 function hotelCost(days){
-    var totalCost= 0;
-    if(days<=10){
-       totalCost= days*100;
-    } else if (days<=20) {
-        firstCost= 10*100;
-        remaining= days-10;
-        secondCost= remaining*80;
-        totalCost= firstCost+secondCost;
-    } else {
-        firstCost= 10*100;
-        secondCost= 10*80;
-        remaining= days-20;
-        thirdCost= remaining*50;
-        totalCost= firstCost+secondCost+thirdCost;
+    if (days > 0){
+        var totalCost= 0;
+        if(days<=10 ){
+            totalCost= days*100;
+         } else if (days<=20) {
+             firstCost= 10*100;
+             remaining= days-10;
+             secondCost= remaining*80;
+             totalCost= firstCost+secondCost;
+         } else {
+             firstCost= 10*100;
+             secondCost= 10*80;
+             remaining= days-20;
+             thirdCost= remaining*50;
+             totalCost= firstCost+secondCost+thirdCost;
+         }
+    }else{
+        console.log('Please enter a valid number of days')
     }
     return totalCost;
-
 }
 
 
-function megaFriend(){                    //var names = ['Raki','', 'hasan', 'jaidnio', 'Rakibul' ];
+function megaFriend(){                    // use var names for this function like this "var names = ['Rakibul', 'Hasan'];"
     var max = names[0];
      for(var i=0; i<names.length; i++){
             var box = names[i];
@@ -40,8 +55,4 @@ function megaFriend(){                    //var names = ['Raki','', 'hasan', 'ja
     }
     return max;
 }
-                                           //console.log(megaFriend(names));
-console.log(kilometerToMeter(-1.2));
-console.log(budgetCalculator(1, 1, 1));
-console.log(hotelCost(21));
-console.log(megaFriend(names));
+                                          
